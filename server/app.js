8f1a38249d8162764b61c024d2736b37a95d7bea
SALMA EL HANI <salmaelhani7@gmail.com>
2024-05-23 22:08:32 +0100

    update app and index files

--- server/app.js
+++ server/app.js
@@ -2,16 +2,15 @@ import express from "express";
 const app = express();
 import db from "./models/index.js";
 
-
 const startserver = async () =>{
     try{ 
         await db.mongoose.connect(db.url, {
-            dbName : "cleanSpace"        
+            dbName : ""        
         });
         console.log("Connection to the database successful");
         
-        app.listen(db.PORT,'0.0.0.0',()=>{
-            console.log("http://localhost:"+db.PORT);
+        app.listen(db.PORT,()=>{
+            console.log("http://localhost:"+db.PORT);   
         })  
     }catch(error){
         console.log(error.message);
